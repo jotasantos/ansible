5987d415be7480877f71df548c50d7bceef358ba
Jaime Santos Amandi <jaime.santos.amandi@oracle.com>
2021-03-21 17:05:04 +0000

    change-to-functions3

--- mgmt-dhcp/pyscripts/client_ips.py
+++ mgmt-dhcp/pyscripts/client_ips.py
@@ -21,11 +21,11 @@ def arp_to_list():
 		res_list_ips.append(res[i]['ip'])
 	# remove from the list the gateway and 192.x addresses from the list
 	res_list_ips = [i for i in res_list_ips if not (regex1.match(i) or regex2.match(i))]
-	return res_list_ips
+return res_list_ips
 
 def arp_to_host_file(arp_list):
-    filel='/etc/hosts' 
-	#hosts_result = {}
+    filel = '/etc/hosts' 
+	hosts_result = {}
 	fileh = open(filel, 'w')
 	fileh.write('127.0.0.1	localhost\n127.0.1.1	ubuntu1804-pfne\nff02::1 ip6-allnodes\nff02::2 	ip6-allrouters\n')
 	for i in range(len(arp_list)):
