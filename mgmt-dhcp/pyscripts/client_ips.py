739ea7c7a1e7613859e6a890dd8bce7b699f0bf0
Jaime Santos Amandi <jaime.santos.amandi@oracle.com>
2021-06-20 19:05:55 +0200

    junos_support-10

--- mgmt-dhcp/pyscripts/client_ips.py
+++ mgmt-dhcp/pyscripts/client_ips.py
@@ -35,19 +35,21 @@ def arp_to_host_file(arp_list):
 	fileh.write('127.0.0.1	localhost\n127.0.1.1	ubuntu1804-pfne\nff02::1 ip6-allnodes\nff02::2 	ip6-allrouters\n')
 	for i in range(len(arp_list)):
 		try:
-			device_ios = driver_ios(arp_list[i], 'ansible', 'ansible')
+			ip = arp_list[i]
+			device_ios = driver_ios(ip, 'ansible', 'ansible')
 			device_ios.open()
-			fileh.write(arp_list[i] + " " + device_ios.get_facts()['fqdn'] + '\n')
+			fileh.write(ip + " " + device_ios.get_facts()['fqdn'] + '\n')
 			device_ios.close()
-		except NetMikoAuthenticationException:
-			print('IOS wrong OS or not responding')
+		except :
+			print(f'IOS wrong OS or not responding for {ip}')
 		try:
-			device_junos = driver_junos(arp_list[i], 'ansible', 'ansible')
+			ip = arp_list[i]
+			device_junos = driver_junos(ip, 'ansible', 'ansible')
 			device_junos.open()
-			fileh.write(arp_list[i] + " " + device_junos.get_facts()['fqdn'] + '\n')
+			fileh.write(ip + " " + device_junos.get_facts()['fqdn'] + '\n')
 			device_junos.close()
-		except NetMikoAuthenticationException:
-			print('Junos wrong OS or not responding')
+		except:
+			print('Junos and IOS wrong OS or not responding')
 	fileh.close()
 
 if __name__ == '__main__':
