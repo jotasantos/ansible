479673a0cdf521c0942796400e2fac85ffe46510
jotasantos <jaimesantos23@gmail.com>
2021-07-23 15:34:31 +0100

    Test junos napalm (#2)
    * junos_support-1
    
    * junos_support-2
    
    * junos_support-3
    
    * junos_support-4
    
    * junos_support-5
    
    * junos_support-6
    
    * junos_support-7
    
    * junos_support-8
    
    * junos_support-9
    
    * junos_support-10
    
    * junos_support-11
    
    * junos_support-12
    
    * sd
    
    Co-authored-by: Jaime Santos Amandi <jaime.santos.amandi@oracle.com>

--- mgmt-dhcp/pyscripts/client_ips.py
+++ mgmt-dhcp/pyscripts/client_ips.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.6
 from netmiko import ConnectHandler
+from netmiko import NetMikoAuthenticationException
+from netmiko import NetmikoTimeoutException
 from napalm import get_network_driver
 import re
 
@@ -8,32 +10,46 @@ USER="ansible"
 PASSWORD="ansible"
 regex1 = re.compile("^192\.168\.222\.\d{1,3}$")
 regex2 = re.compile("^10\.8\.11\.1$")
-get_network_driver('ios')
-driver = get_network_driver('ios')
-device = driver('192.168.222.2', 'ansible', 'ansible')
-device.open()
+#get_network_driver('ios')
+driver_ios = get_network_driver('ios')
+driver_junos = get_network_driver('junos')
+device_mgmt_rt = driver_ios('192.168.222.2', 'ansible', 'ansible')
+device_mgmt_rt.open()
 
 def arp_to_list():
-	res=device.get_arp_table()
+	res=device_mgmt_rt.get_arp_table()
 	res_list_ips = []
 	for i in range(len(res)):
 		res_list_ips.append(res[i]['ip'])
 	# remove from the list the gateway and 192.x addresses from the list
 	res_list_ips = [i for i in res_list_ips if not (regex1.match(i) or regex2.match(i))]
-	print(res_list_ips) 
+	device_mgmt_rt.close()
+	print(res_list_ips)
 	return res_list_ips
+
+
 def arp_to_host_file(arp_list):
     #filel = '/etc/hosts' 
 	hosts_result = {}
 	fileh = open("/etc/hosts", 'w')
 	fileh.write('127.0.0.1	localhost\n127.0.1.1	ubuntu1804-pfne\nff02::1 ip6-allnodes\nff02::2 	ip6-allrouters\n')
 	for i in range(len(arp_list)):
-		device1 = driver(arp_list[i], 'ansible', 'ansible')
-		device1.open()
-		#hosts_result[arp_list[i]] = device1.get_facts()['fqdn']
-		#print(device1.get_facts()['fqdn']+arp_list[i])
-		fileh.write(arp_list[i] + " " + device1.get_facts()['fqdn'] + '\n')
-		device1.close()
+		try:
+			ip = arp_list[i]
+			device_ios = driver_ios(ip, 'ansible', 'ansible')
+			device_ios.open()
+			fileh.write(ip + " " + device_ios.get_facts()['hostname'] + '\n')
+			device_ios.close()
+		except :
+			print(f'Not responding as IOS for {ip}')
+		try:
+			ip = arp_list[i]
+			device_junos = driver_junos(ip, 'ansible', 'Ansible')
+			device_junos.open()
+			fileh.write(ip + " " + device_junos.get_facts()['fqdn'] + '\n')
+			device_junos.close()
+		except:
+			print(f'Not responding as JUNOS for {ip}')
 	fileh.close()
 
 if __name__ == '__main__':
