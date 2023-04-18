71e068bffa908273bce3b4db254d2f149d7c1fc4
JAIME <jaimesantos23@gmail.com>
2023-04-18 16:53:16 +0100

    more-py-1

--- tools/pingcheck.sh
+++ tools/pingcheck.sh
@@ -3,6 +3,7 @@ date
 cat ./pingchecklist-$1.txt |  while read output
 do
     ping -c 1 "$output" > /dev/null
+    # $? in bash is the status of the last executed program
     if [ $? -eq 0 ]; then
     echo "node $output is up" 
     else
